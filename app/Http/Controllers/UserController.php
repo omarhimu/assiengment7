41347706b3c93c5e9d25d1e44f7edccd79c756ca
Md. Omar Sharif <59568861+omarhimu@users.noreply.github.com>
2023-11-11 23:37:04 +0600

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function testing(){
+        echo "<h2>Hello, Laravel!</h2>";
+    }
+}
